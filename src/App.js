d296e5455417f95fcd705aadc5b44751fc30a710
LarryAustinIyere <austinlarry1506@gmail.com>
2022-11-04 22:38:19 +0100

    updating our directory page with directory component and category item copmponent

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const categories = [
     {
       "id": 1,
-      "title": "hats",
+      "title": "Hats",
       "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
